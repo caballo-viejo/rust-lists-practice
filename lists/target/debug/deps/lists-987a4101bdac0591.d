903f94ee4ab5224288de6a777e5d49435ca7b314
funk-insurance <angellechugagonzalez@gmail.com>
2025-02-13 19:39:30 -0700

    2.5

---
+++ lists/target/debug/deps/lists-987a4101bdac0591.d
@@ -0,0 +1,6 @@
+C:\Users\mrcla\Documents\projects\rust-lists-practice\lists\target\debug\deps\liblists-987a4101bdac0591.rmeta: src\lib.rs src\first.rs
+
+C:\Users\mrcla\Documents\projects\rust-lists-practice\lists\target\debug\deps\lists-987a4101bdac0591.d: src\lib.rs src\first.rs
+
+src\lib.rs:
+src\first.rs:
---
+++ lists/target/debug/deps/lists-d1adf5577c12e1cc.d
@@ -0,0 +1,6 @@
+C:\Users\mrcla\Documents\projects\rust-lists-practice\lists\target\debug\deps\liblists-d1adf5577c12e1cc.rmeta: src\lib.rs src\first.rs
+
+C:\Users\mrcla\Documents\projects\rust-lists-practice\lists\target\debug\deps\lists-d1adf5577c12e1cc.d: src\lib.rs src\first.rs
+
+src\lib.rs:
+src\first.rs:
